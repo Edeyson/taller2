4cc2e39ca07b85ae8bd2de55e387856d986e461d
german <gerasman827@gmail.com>
2020-11-14 15:07:05 -0500

    hla yo

--- src/app/components/triqui/triquiplay/triquiplay.component.ts
+++ src/app/components/triqui/triquiplay/triquiplay.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TrikiService } from '../../../services/triki.service';
 
 @Component({
   selector: 'app-triquiplay',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./triquiplay.component.css']
 })
 export class TriquiplayComponent implements OnInit {
+  @Input() jugadorActual: string;
 
-  constructor() { }
+  constructor(private trikiService: TrikiService) { }
 
   ngOnInit(): void {
+    this.jugadorActual = this.trikiService.getJugadorActual();
   }
+
+
 
 }
